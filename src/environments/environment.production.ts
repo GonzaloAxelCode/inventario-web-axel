d73a2f70e034cebae0e931465ec92ca48092342d
GonzaloAxelCode <gonzaloaxelcode@gmail.com>
2025-08-03 22:33:04 -0500

    Observando el peso del proyecto

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	URL_BASE: 'https://samuback-qdb1.onrender.com',
-	URL_BASE_FRONT: 'https://samusystem.vercel.app',
+	URL_BASE: 'https://backend-accesories.onrender.com',
+	URL_BASE_FRONT: 'https://inventario-web-axel-drab.vercel.app',
 	API_VERSION: '1.0',
 };
